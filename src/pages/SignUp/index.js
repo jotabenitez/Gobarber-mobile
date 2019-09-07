570c25aee3b33ebc2d9aace9d405f2b6843d2446
jota <jota@criptext.com>
2019-09-07 13:53:37 -0300

    components button and input - icone de input nao aparece

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Background from '~/components/Background';
+// import { Container } from './styles';
+
+export default function SignUp() {
+  return (
+    <Background>
+      <Text>SignUp</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+      <Text>SignIn</Text>
+    </Background>
+  );
+}
