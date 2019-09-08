79f71b7b7c45a536e8f9772e31235555a3e05724
jota <jota@criptext.com>
2019-09-08 10:25:01 -0300

    sign in page - fix style, icones nao aparecem

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,8 +6,9 @@ import { Container, TInput } from './styles';
 
 function Input({ style, icon, ...rest }, ref) {
   return (
-    <Container style="style">
-      {icon && <Icon name={icon} size={20} color="rgba(255,255,255, 0.6)" />}
+    <Container style={style}>
+      {icon && <Icon name={icon} size={20} color="rgba(255,255,255, 0.8)" />}
+      // eslint-disable-next-line react/jsx-props-no-spreading
       <TInput {...rest} ref={ref} />
     </Container>
   );
